a918f7a7e60d6998ac6f0ea8cf2b04dd3f0a6ded
Efim Sirotkin <efim.sirotkin@gmail.com>
2020-10-02 10:20:39 +0300

    rewritten algorithm

--- HopfieldNetwork.java
+++ HopfieldNetwork.java
@@ -1,101 +1,55 @@
 package sample;
 
-import org.ejml.simple.SimpleMatrix;
-import sun.java2d.pipe.SpanShapeRenderer;
-
-import java.util.ArrayList;
+import java.awt.*;
 
 public class HopfieldNetwork {
+    public static int IMAGE_SIZE = 10;
+    public static int WEIGHT_SIZE = IMAGE_SIZE * IMAGE_SIZE;
+    public static int NUMBER_OF_ITERATIONS = 10;
+    private int[][] weightMatrix;
 
-    public static int NUMBER_OF_ROWS = 1;
-    public static int NUMBER_OF_COLUMNS = 4;
-    public static int NUMBER_OF_IMAGES = 3;
-
-    private SimpleMatrix firstImage;
-    private SimpleMatrix secondImage;
-    private SimpleMatrix thirdImage;
-    private SimpleMatrix weightMatrix;
-
-    public SimpleMatrix getFirstImage() {
-        return firstImage;
+    public HopfieldNetwork(int[][] weightMatrix) {
+        this.weightMatrix = weightMatrix;
     }
 
-    public SimpleMatrix getSecondImage() {
-        return secondImage;
-    }
-
-    public SimpleMatrix getWeightMatrix() {
-        return weightMatrix;
-    }
-
-    public SimpleMatrix getThirdImage() {
-        return thirdImage;
-    }
-
-    public HopfieldNetwork() {
-        firstImage = new SimpleMatrix(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
-        secondImage = new SimpleMatrix(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
-        thirdImage = new SimpleMatrix(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
-        weightMatrix = new SimpleMatrix(NUMBER_OF_COLUMNS, NUMBER_OF_COLUMNS);
-    }
-
-    public void setFirstImage(double[][] sourceArray) {
-        this.firstImage = new SimpleMatrix(sourceArray);
-    }
-
-    public void setSecondImage(double[][] sourceArray) {
-        this.secondImage = new SimpleMatrix(sourceArray);
-    }
-
-    public void setThirdImage(double[][] sourceArray) {
-        this.thirdImage = new SimpleMatrix(sourceArray);
-    }
-
-    public SimpleMatrix[] allocateWeightMatrices(int numberOfMatrices, int numberOfRows, int numberOfColumns)
+    public HopfieldNetwork()
     {
-        SimpleMatrix[] tempMatricesArray = new SimpleMatrix[numberOfMatrices];
-        for(int i = 0; i < tempMatricesArray.length; ++i)
-            tempMatricesArray[i] = new SimpleMatrix(numberOfRows, numberOfColumns);
-
-        return tempMatricesArray;
+        this.weightMatrix = new int[IMAGE_SIZE][IMAGE_SIZE];
     }
 
-    public void calculateWeightsMatrix()
-    {
-        SimpleMatrix[] weightMatricesArray = allocateWeightMatrices(NUMBER_OF_IMAGES, NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
-        weightMatricesArray[0] = getFirstImage().transpose().mult(getFirstImage());
-        weightMatricesArray[1] = getSecondImage().transpose().mult(getSecondImage());
-        weightMatricesArray[2] = getThirdImage().transpose().mult(getThirdImage());
+    public void addImage(int[] sourceImage) {
+        int[][] tempWeightsMatrix = new int[IMAGE_SIZE][IMAGE_SIZE];
 
-        for(int i = 0; i < weightMatricesArray.length; ++i)
-            weightMatrix = weightMatrix.plus(weightMatricesArray[i]);
-
-        zeroWeightMatrixDiagonal();
-    }
-
-    public SimpleMatrix getMultipliedTestImageByWeights(SimpleMatrix testImage)
-    {
-        return weightMatrix.mult(testImage.transpose());
-    }
-
-    public SimpleMatrix getResultImage(SimpleMatrix multipliedImage)
-    {
-        SimpleMatrix testMatrix = new SimpleMatrix(multipliedImage);
-        for(int i = 0; i < multipliedImage.numRows(); ++i)
-            for(int j = 0; j < multipliedImage.numCols(); ++j)
-            {
-                if(testMatrix.get(i,j)>= 0)
-                    testMatrix.set(i,j, 1);
-                else if(testMatrix.get(i,j) < 0)
-                    testMatrix.set(i,j,-1);
+        for (int i = 0; i < IMAGE_SIZE; ++i) {
+            for (int j = 0; j < IMAGE_SIZE; ++j) {
+                if(i == j) {
+                    continue;
+                }
+                weightMatrix[i][j] += sourceImage[i] * sourceImage[j];
             }
-         return testMatrix.transpose();
+        }
     }
 
-    public void zeroWeightMatrixDiagonal()
+    public int[] recognizeImage(int[] testImage)
     {
-        for(int i = 0; i < NUMBER_OF_COLUMNS; ++i)
-            weightMatrix.set(i,i,0.0);
+        int[] outputImage = new int[IMAGE_SIZE];
+        int testImageValue;
+        for(int i = 0;i < IMAGE_SIZE; ++i) {
+            testImageValue = 0;
+            for (int j = 0; j < IMAGE_SIZE; ++j) {
+                testImageValue += testImage[j] * this.weightMatrix[j][i];
+            }
+            outputImage[i] = this.signum(testImageValue);
+        }
+        return outputImage;
+    }
+
+    public int signum(int sourceValue)
+    {
+        if(sourceValue >= 0)
+            return 1;
+        else
+            return -1;
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,40 +1,130 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.ejml.simple.SimpleMatrix;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
 
-public class Main extends Application {
+import static sample.HopfieldNetwork.IMAGE_SIZE;
+import static sample.HopfieldNetwork.NUMBER_OF_ITERATIONS;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        //primaryStage.show();
-        double[][] firstImage = {{-1.0, 1.0, -1.0, 1.0}};
-        double[][] secondImage = {{ 1.0, -1.0, 1.0, 1.0}};
-        double[][] thirdImage = {{-1.0, 1.0, -1.0, -1.0}};
+class Main {
 
-        double[][] testImage = {{ 1.0, -1.0, 1.0, -1.0}};
 
-        HopfieldNetwork hopfieldNetwork = new HopfieldNetwork();
-        hopfieldNetwork.setFirstImage(firstImage);
-        hopfieldNetwork.setSecondImage(secondImage);
-        hopfieldNetwork.setThirdImage(thirdImage);
+    public static int[] firstImage = {-1, -1, -1, -1, 1, 1, 1, 1, 1, 1};
+    public static int[] secondImage = {-1, 1, 1, 1, 1, 1, 1, 1, 1, -1};
+    public static int[] thirdImage = {1, 1, 1, 1, -1, -1, 1, 1, 1, 1};
 
-        hopfieldNetwork.calculateWeightsMatrix();
+    public static int[] testImage = {-1, 1, 1, 1, -1, 1, 1, -1, -1, -1};
 
-        System.out.println(hopfieldNetwork.getWeightMatrix());
+    public static int MAX_NOISE = 90;
 
-        System.out.println(hopfieldNetwork.getResultImage(hopfieldNetwork.getMultipliedTestImageByWeights(new SimpleMatrix(testImage))));
+    public static boolean isEqual(int[] firstImage, int[] secondImage) {
+        for (int i = 0; i < firstImage.length; ++i) {
+            if (firstImage[i] != secondImage[i])
+                return false;
+        }
+        return true;
     }
 
+    public static void printImage(int[] sourceImage) {
+        for (int i = 0; i < IMAGE_SIZE; ++i) {
+            if (sourceImage[i] > 0)
+                System.out.print("x ");
+            else
+                System.out.print("o ");
+        }
+
+        System.out.println();
+    }
+
+    public static int[] generateNoisedImage(int[] primaryImage, double noiseDegree) {
+        int[] tempImage = primaryImage;
+        for (int i = 0; i < IMAGE_SIZE; ++i) {
+            if (Math.random() < noiseDegree)
+                tempImage[i] *= -1;
+        }
+        return tempImage;
+
+    }
+
+    public static double roundNumber(double primaryValue, int scale) {
+        BigDecimal bd = new BigDecimal(primaryValue);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
 
     public static void main(String[] args) {
-        launch(args);
+        HopfieldNetwork hopfieldNetwork = new HopfieldNetwork();
+        int[] outputImage = new int[IMAGE_SIZE];
+        double noise = 0.0;
+
+
+        hopfieldNetwork.addImage(firstImage);
+        hopfieldNetwork.addImage(secondImage);
+        hopfieldNetwork.addImage(thirdImage);
+
+        System.out.println("Исходные образы: ");
+        printImage(firstImage);
+        printImage(secondImage);
+        printImage(thirdImage);
+        System.out.println("----------------");
+
+        System.out.println("Тестовый образ: ");
+        printImage(firstImage);
+
+        for (int i = 0; i < MAX_NOISE; i += 10) {
+            int[] noisedImage = generateNoisedImage(firstImage, noise);
+            System.out.println("Шум " + noise * 100 + "%");
+            printImage(noisedImage);
+            outputImage = noisedImage;
+            for (int j = 0; j < NUMBER_OF_ITERATIONS; ++j) {
+                outputImage = hopfieldNetwork.recognizeImage(outputImage);
+                if (isEqual(outputImage, firstImage) || isEqual(outputImage, secondImage) || isEqual(outputImage, thirdImage))
+                    break;
+            }
+            System.out.println("Распознанный образ: ");
+            printImage(outputImage);
+            noise += 0.1;
+            noise = roundNumber(noise, 2);
+            System.out.println("\n");
+        }
     }
 }
+
+//    public static int[] firstImage = {
+//            -1, -1, -1,-1, -1, -1, -1, -1, -1, -1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    };
+//    public static int[] secondImage = {
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    };
+//    public static int[] thirdImage = {
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+//    };
+//
